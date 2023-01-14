df6d4baff392a08d5f94cff28ff2299b4bd95fd9
Mario Döring <deluna.yy@gmail.com>
2023-01-14 19:17:04 +0100

    Added framebuffer renderbuffer attachments

--- src/Graphics/Framebuffer.php
+++ src/Graphics/Framebuffer.php
@@ -5,6 +5,13 @@ namespace VISU\Graphics;
 class Framebuffer extends AbstractFramebuffer
 {
     /**
+     * An array of renderbuffet attachments
+     * 
+     * @var array<int, int> maps: attachment -> render buffer object
+     */
+    private array $renderbufferAttachments = [];
+
+    /**
      * Creates a new framebuffer object
      * 
      * @param GLState $gl
@@ -21,5 +28,24 @@ class Framebuffer extends AbstractFramebuffer
     final public function __destruct()
     {
         glDeleteFramebuffers(1, $this->id);
+    }
+
+    /**
+     * Creates a render buffer attachent
+     */
+    public function createRenderbufferAttachment(int $format, int $attachment, int $width, int $height)
+    {
+        // delete and remove an old renderbuffer attachment when it already exists
+        if (isset($this->renderbufferAttachments[$attachment])) {
+            glDeleteRenderbuffers(1, $this->renderbufferAttachments[$attachment]);
+            unset($this->renderbufferAttachments[$attachment]);
+        }   
+
+        glGenRenderbuffers(1, $rbo);
+        glBindRenderbuffer(GL_RENDERBUFFER, $rbo);
+        glRenderbufferStorage(GL_RENDERBUFFER, $format, $width, $height);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, $attachment, GL_RENDERBUFFER, $rbo);
+
+        $this->renderbufferAttachments[$attachment] = $rbo;
     }
 }
